d4f9ee8d2cdadb90ac537b8f8daf005a85d0b617
kongsachakyarith <kongsachakyarith@gmail.com>
2025-08-19 00:18:56 +0700

    change gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,24 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.3" apply false
-    id("io.spring.dependency-management") version "1.0.8.RELEASE" apply false
-    kotlin("jvm") version "1.7.0" apply false
-    kotlin("plugin.spring") version "1.7.0" apply false
+    id("org.springframework.boot") version "3.5.4" apply false
+    id("io.spring.dependency-management") version "1.1.6" apply false
+    kotlin("jvm") version "1.9.24" apply false
+    kotlin("plugin.spring") version "1.9.24" apply false
 }
-
 
 allprojects {
     group = "org.kshrd.cloud"
     version = "1.0.0"
 
     tasks.withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
+        // JDK 21
+        sourceCompatibility = JavaVersion.VERSION_21.toString()
+        targetCompatibility = JavaVersion.VERSION_21.toString()
     }
 
     tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+        kotlinOptions.jvmTarget = "21"
     }
 }
 
@@ -35,16 +35,3 @@ subprojects {
 repositories {
     mavenCentral()
 }
-//
-dependencies {
-//    testImplementation(kotlin("test"))
-
-}
-
-//tasks.test {
-//    useJUnitPlatform()
-//}
-//
-//tasks.withType<KotlinCompile> {
-//    kotlinOptions.jvmTarget = "1.8"
-//}
